58a01a6d538d8384caffc62c68662996ba635158
Punin06 <90221164+Punin06@users.noreply.github.com>
2025-10-01 07:08:55 -0400

    Update imdb_movie_scrapper.py

--- imdb_movie_scrapper.py
+++ imdb_movie_scrapper.py
@@ -4,101 +4,99 @@ from bs4 import BeautifulSoup
 import time
 import re
 import pandas as pd
+from tkinter import filedialog
+from tkinter import *
 
-#this displays the top 250 movies on imdb
+root = Tk()
+root.withdraw()
 
-def main():
-    url = "https://www.imdb.com/chart/top/"
-    #create the browser obj
-    driver = Safari()
-    #pass the site in the browser obj
-    driver.get(url)
-    #adding a wait, so the page can fully load up.
-    time.sleep(10)
-    #creating soup obj using driver page source
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    #selecting all the movie's names
-    movie_names = soup.select("li h3")
-    #finding all the movie's ratings
-    ratings = soup.find_all("span" , attrs = {"data-testid": "ratingGroup--imdb-rating"})
-    #finding all the movie's released year
-    movie_year = soup.find_all("span", attrs = {"class": "sc-be6f1408-8 fcCUPU cli-title-metadata-item"})
+url = "https://www.imdb.com/chart/top/"
+#create the browser obj
+driver = Safari()
+#pass the site in the browser obj
+driver.get(url)
+#add a wait, so the page can fully load up, this way I can retrieve everything, sometimes things might be missing cuz the page hasn't loaded, completely.
+time.sleep(5)
+#than pass driver.page_source to pass the data in the page
+soup = BeautifulSoup(driver.page_source, "html.parser") 
+movie_names = soup.select("li h3")
+ratings = soup.find_all("span" , attrs = {"data-testid": "ratingGroup--imdb-rating"})
+movie_year = soup.find_all("span", attrs = {"class":"sc-15ac7568-7 cCsint cli-title-metadata-item"})
 
-    #A count that is used for seperating runtime and released year 
-    count = 1
-    #variable to store movie released years
-    years = []
-    #this one is to store the runtimes
-    runtime = []
+#A count that is used for seperating runtime and released year 
+count = 1
+#variable to store movie released years
+years = []
+#this one is to store the runtimes
+runtime = []
 
-    #A placeholder for the movie year
-    tyear = ""
-    #A placeholder for the runtime
-    temp1 = ""
-    #stores movies ranking number
-    number = []
-    #stores name of movie
-    title = []
-    #stores imdb rating of movie
-    rating = []
-    #stores imdb number of votes
-    votes = []
-    #dataframe to store the movie's number, title, runtime, rating and votes
-    df = pd.DataFrame()
+#A placeholder for the movie year
+tyear = ""
+#A placeholder for the runtime
+temp1 = ""
+#stores movies ranking number
+number = []
+#stores name of movie
+title = []
+#stores imdb rating of movie
+rating = []
+#stores imdb number of votes
+votes = []
+#dataframe to store the movie's number, title, runtime, rating and votes
+df = pd.DataFrame()
 
 #seperating the movie released year and runtime, and appending them.
-    for year in movie_year:
-        tyear+=year.text + " "
-        if (count%3==0):
-            tyear = tyear.split()
-            for t in tyear:
-                if(re.findall("[0-9]{4}", t)):
-                    years.append(t)
-                    
-                elif(re.findall("h|m$", t)):
-                    temp1 += t + " "
+for year in movie_year:
+    tyear+=year.text + " "
+    if (count%3==0):
+        tyear = tyear.split()
+        for t in tyear:
+            if(re.findall("[0-9]{4}", t)):
+                years.append(t)
+                
+            elif(re.findall("h|m$", t)):
+                temp1 += t + " "
 
-                elif not (re.search("(^[A-Z])", t)):
-                    runtime.append("N/A")
-            
-            runtime.append(temp1)
-            temp1 = ""
-            tyear = ""
-        count+=1
-    
-    #Seperating votes and appending them
-    for rate in ratings: 
-        temp = rate.text
-        temp = temp.split()
-        votes.append(temp[1])
-        rating.append(temp[0])
+            elif not (re.search("([0-9]h.*)|([0-9]+m)|(^[A-Z])", t)):
+                runtime.append("N/A")
         
-    #Seperating movie name and appending them
-    for num, name in enumerate(movie_names):
-        name = re.sub("^[0-9]+.", "", name.text)
-        title.append(name)
-        number.append(num + 1)
+        runtime.append(temp1)
+        temp1 = ""
+        tyear = ""
+    count+=1
 
-    #Assigning to dataframe columns
-    df["Movie Ranking"] = number
-    df["Movie Name"] = title
-    df["Released Year"] = years
-    df["Runtime"] =runtime
-    df["Rating"] = rating
-    df["Votes"] = votes
-
-    driver.quit()
-
-    print("Saving data to IMDB top 250 movies.csv")
-    #saving dataframe to csv file
-    df.to_csv("IMDB top 250 movies.csv", index = False)
-    print("Data Saved, now displaying entries!")
-    file = pd.read_csv("IMDB top 250 movies.csv", sep = ",")
-    num_of_movies = int(input("Enter number of top movies to show!\n"))
-    pd.set_option("display.max_columns", None)
-    print(file.head(num_of_movies))
-        
-
-if __name__ =="__main__":
-    main()
+#Seperating votes and appending them
+for rate in ratings:
+    #t = j.find("span" , attrs = {"data-testid": "ratingGroup--imdb-rating"}).text   
+    temp = rate.text
+    temp = temp.split()
+    votes.append(temp[1])
+    rating.append(temp[0])
     
+#Seperating movie name and appending them
+for num, name in enumerate(movie_names):
+    name = re.sub("^[0-9]+.", "", name.text)
+    title.append(name)
+    number.append(num + 1)
+
+while len(runtime)<len(number):
+    runtime.append("0")
+    
+#Assigning to dataframe columns
+df["Movie Number"] = number
+df["Movie Name"] = title
+df["Released Year"] = years
+df["Runtime"] =runtime
+df["Rating"] = rating
+df["Votes"] = votes
+
+driver.quit()
+
+# Ask the user to select a folder
+folder_selected = filedialog.askdirectory()
+
+#Use the selected folder to set the save location
+#saving dataframe to csv file
+df.to_csv(folder_selected + "/" +"IMDB top 250 movies.csv", index = False)
+
+root.mainloop()
